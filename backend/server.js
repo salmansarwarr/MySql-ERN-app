1aeb9a52ad5f6f6d5dbf7682dccb097bfb033b73
salmansarwarr <muhammadsalmansarwar32@gmail.com>
2023-03-02 14:58:05 +0500

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,38 @@
+const path = require("path");
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const dotenv = require("dotenv").config();
-const colors = require('colors');
+const colors = require("colors");
 const goalsRouter = require("./routes/goals");
 const usersRouter = require("./routes/user");
-const {errorHandler} = require("./middleware/error");
-const connectDB = require('./config/db');
+const { errorHandler } = require("./middleware/error");
+const connectDB = require("./config/db");
 const port = process.env.PORT;
 
 connectDB();
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/goals", goalsRouter);
 app.use("/api/users", usersRouter);
+
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler);
 
--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../redux/auth/authSlice';
+import goalReducer from '../redux/goals/goalSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    goals: goalReducer
   },
 });
