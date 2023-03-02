ef16f34fd80eb33ae4cf5bc730c04d3b54779744
salmansarwarr <muhammadsalmansarwar32@gmail.com>
2023-03-02 17:58:11 +0500

    d

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("*", (req, res) =>
         res.sendFile(
-            path.join(__dirname, "./frontend/build/index.html"), (err) => {
+            path.join("./frontend/build/index.html"), (err) => {
                 res.status(500).send(err);
             }
         )
