9dd1486c03cc4c0648d49d51eede76ff58ff2362
salmansarwarr <muhammadsalmansarwar32@gmail.com>
2023-03-02 17:44:59 +0500

    fix error

--- server.js
+++ server.js
@@ -21,11 +21,13 @@ app.use("/api/users", usersRouter);
 
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.use(express.static(path.join(__dirname, "./frontend/build")));
 
     app.get("*", (req, res) =>
         res.sendFile(
-            path.resolve(__dirname, "../", "frontend", "build", "index.html")
+            path.join(__dirname, "./frontend/build/index.html"), (err) => {
+                res.status(500).send(err);
+            }
         )
     );
 } else {
